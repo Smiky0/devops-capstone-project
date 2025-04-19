bb48a2dbae452557f49e105bc07d4d847dc76b2b
Soumik <smik1172@gmail.com>
2025-04-19 18:53:30 +0530

    Added CORS header

--- tests/__init__.py
+++ tests/__init__.py
@@ -3,8 +3,3 @@ from flask_talisman import Talisman
 
 app = Flask(__name__)
 talisman = Talisman(app)
-
-# expose `talisman` to allow importing it elsewhere
-__all__ = ['talisman', 'app']
-
-talisman = talisman
